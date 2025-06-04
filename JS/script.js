4b4787b0b8ecb6842c2b5d5982c993b92997b13b
Cruel0224 <badboylimmo22@gmail.com>
2025-06-04 16:05:16 +0300

    update file

--- JS/script.js
+++ JS/script.js
@@ -1,9 +1,12 @@
 'use strict';
 
+// We invite the user to enter three numbers through a prompt
 const addNum1 = +prompt("Напиши перше число!");
 const addNum2 = +prompt("Напиши друге число!");
 const addNum3 = +prompt("Напиши третє число!");
 
+// We calculate the arithmetic mean
 const average = (addNum1 + addNum2 + addNum3) / 3;
 
+// Deriving the average using alert
 alert("Середнє арифметичне число: " + average);
