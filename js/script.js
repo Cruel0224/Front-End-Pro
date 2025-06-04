8aa9c981249873bada2b13e7590cca9b330ef3c1
Cruel0224 <128542325+Cruel0224@users.noreply.github.com>
2025-06-04 21:28:04 +0300

    Add files via upload

---
+++ js/script.js
@@ -0,0 +1,12 @@
+'use strict';
+
+// We invite the user to enter three numbers through a prompt
+const addNum1 = +prompt("Напиши перше число!");
+const addNum2 = +prompt("Напиши друге число!");
+const addNum3 = +prompt("Напиши третє число!");
+
+// We calculate the arithmetic mean
+const averageNumber = (addNum1 + addNum2 + addNum3) / 3;
+
+// Deriving the average using alert
+alert("Середнє арифметичне число: " + averageNumber);
