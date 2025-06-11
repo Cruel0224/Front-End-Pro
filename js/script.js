39390a0505d74cbd1227bd493ae4adc083f1814d
Cruel0224 <128542325+Cruel0224@users.noreply.github.com>
2025-06-11 21:54:54 +0300

    Update script.js

--- js/script.js
+++ js/script.js
@@ -2,6 +2,6 @@
 // The greetUser function, which takes one parameter - 
 // and returns a string like this: "Hello, [NAME]!". I also added quotes.
 function greetUser(name) {
-    return `"Привіт, ${name}`;
+    return `"Привіт, ${name}!"`;
 }
-console.log(greetUser('Влад!"'));
+console.log(greetUser('Влад'));
