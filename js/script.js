a6efb84c09aa3917ff0eb85b4215f7eac7ab47a8
Cruel0224 <128542325+Cruel0224@users.noreply.github.com>
2025-06-11 21:36:56 +0300

    Update script.js

--- js/script.js
+++ js/script.js
@@ -1,6 +1,7 @@
 'use strict';
-
+// The greetUser function, which takes one parameter - 
+// and returns a string like this: "Hello, [NAME]!". I also added quotes.
 function greetUser(name) {
-    return `Привіт, ${name}`;
+    return `"Привіт, ${name}`;
 }
-console.log(greetUser("Влад!"));
+console.log(greetUser('Влад!"'));
