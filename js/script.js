3fde97275fed92e21ca4295d98c5d7e3f1e7a5d8
Cruel0224 <128542325+Cruel0224@users.noreply.github.com>
2025-07-03 21:37:57 +0300

    Delete js directory

--- js/script.js
+++
@@ -1,7 +0,0 @@
-'use strict';
-
-
-const toggleTheme = document.getElementById('toggle-theme');    // Find the button.
-toggleTheme.addEventListener('click',  () => {                  // Hang the click event handler.
-    document.body.classList.toggle('dark-theme');               // Switch the class to body.
-});
