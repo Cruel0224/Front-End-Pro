73d082a1ee154db152e76ae2f6fcd92e3dfeeef6
Cruel0224 <128542325+Cruel0224@users.noreply.github.com>
2025-07-03 14:44:40 +0300

    Update script.js

--- js/script.js
+++ js/script.js
@@ -2,6 +2,6 @@
 
 
 const toggleTheme = document.getElementById('toggle-theme');    // Find the button.
-toggleTheme.addEventListener('click',  () => {                 // Hang the click event handler.
-    document.body.classList.toggle('dark-theme');                               // Switch the class to body.
+toggleTheme.addEventListener('click',  () => {                  // Hang the click event handler.
+    document.body.classList.toggle('dark-theme');               // Switch the class to body.
 });
