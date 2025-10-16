4ba45b57362f7b4529f2569c806b717bdda300c5
Cruel0224 <128542325+Cruel0224@users.noreply.github.com>
2025-10-17 01:24:13 +0300

    Update FavoritesList.jsx

--- src/components/FavoritesList.jsx
+++ src/components/FavoritesList.jsx
@@ -1,16 +1,31 @@
 import React from "react";
 
 function FavoritesList({ favorites, onSelectCity, onRemove }) {
-    if (favorites.length === 0) return null;
+    if (!favorites || favorites.length === 0) return null;
 
     return (
-        <div className="favorites">
-            <h3>Обрані міста:</h3>
-            <ul>
-                {favorites.map((city) => (
-                    <li key={city}>
-                        <span onClick={() => onSelectCity(city)}>{city}</span>
-                        <button onClick={() => onRemove(city)}>×</button>
+        <div className="favorites-list mt-4">
+            <h3>Обрані міста</h3>
+            <ul className="list-group">
+                {favorites.map((cityObj, i) => (
+                    <li
+                        key={i}
+                        className="list-group-item d-flex justify-content-between align-items-center"
+                    >
+            <span
+                style={{ cursor: "pointer" }}
+                onClick={() => onSelectCity(cityObj)}
+            >
+              {cityObj.city}, {cityObj.country}
+                {cityObj.admin1 ? `, ${cityObj.admin1}` : ""}
+            </span>
+
+                        <button
+                            className="btn btn-sm btn-danger"
+                            onClick={() => onRemove(cityObj)}
+                        >
+                            Видалити
+                        </button>
                     </li>
                 ))}
             </ul>
